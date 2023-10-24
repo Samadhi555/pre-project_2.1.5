6ce3292df2a9db328becc6f65b042bc402c37c15
Samadhi555 <9522224962@mail.ru>
2023-10-24 11:56:54 +0300

    Comm2

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
-
-
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,7 +1,5 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
